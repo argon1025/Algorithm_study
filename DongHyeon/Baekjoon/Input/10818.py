6a595ad7b691c847619d3b9072062f859b91bb4b
Daphne-Dev <daphned@naver.com>
2020-09-03 23:30:58 +0900

    11054, 1699, 1912

--- DongHyeon/Baekjoon/Input/10818.py
+++ DongHyeon/Baekjoon/Input/10818.py
@@ -1,11 +1,3 @@
-import sys
-N = int(sys.stdin.readline())
-num = list(map(int, sys.stdin.readline().split()))
-min = num[0]
-max = num[0]
-for i in range(N):
-    if num[i] > max:
-        max = num[i]
-    if num[i] < min:
-        min = num[i]
-print(min, max)
+n = int(input())
+nums = list(map(int, input().split()))
+print(min(nums), max(nums))
